b41f539c575239ead19707d064c9f663fb2505ff
Sabbir Ahmed <sabbir15-4993@diu.edu.bd>
2020-12-09 01:57:24 +0600

    First Commit

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,9 @@
+@include('admin.layouts.navbar')
+@include('admin.layouts.sidebar')
+@include('admin.layouts.content')
+{{--<div class="content-wrapper">--}}
+
+{{--        @yield('content')--}}
+
+{{--    </div>--}}
+@include('admin.layouts.footer')
